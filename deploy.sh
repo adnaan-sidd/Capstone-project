63c4fea0a5c9cb2b3ece4bff19879460d88e06d0
adnaan-sidd <136224753+adnaan-sidd@users.noreply.github.com>
2023-11-11 00:30:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,40 @@
 if ! command -v docker &> /dev/null; then
   # Docker is not installed, so update package manager and install Docker
   echo "Installing Docker..."
-  sudo apt install ca-certificates curl gnupg
+  #!/bin/bash
 
+# Install required packages
+sudo yum install -y ca-certificates curl gnupg
+
+# Create keyrings directory
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Add Docker GPG key
+curl -fsSL https://download.docker.com/linux/amazon/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+# Add Docker repository to package manager
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo apt install docker-compose -y
+  "deb [arch=$(uname -m | sed 's/x86_64/amd64/')] https://download.docker.com/linux/amazon \
+  $(rpm -E %amzn_platform_version) stable" | \
+  sudo tee /etc/yum.repos.d/docker.list > /dev/null
 
-sudo service docker restart
+# Update package manager
+sudo yum update -y
+
+# Install Docker packages
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Start and enable Docker service
+sudo service docker start
+sudo systemctl enable docker
+
+# Adjust Docker socket permissions
 sudo chmod 666 /var/run/docker.sock
+
+# Restart Docker service
 sudo systemctl restart docker
+
 else
   echo "Docker is already installed."
 fi
