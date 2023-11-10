2ccc6f25c140b021052a79f7518adaeee3487fab
adnaan-sidd <136224753+adnaan-sidd@users.noreply.github.com>
2023-11-11 00:24:54 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,24 @@
 if ! command -v docker &> /dev/null; then
   # Docker is not installed, so update package manager and install Docker
   echo "Installing Docker..."
-  sudo apt update -y
-  sudo apt install docker.io -y
+  sudo apt install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt install docker-compose -y
+
+sudo service docker restart
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
 else
   echo "Docker is already installed."
 fi
@@ -18,6 +34,4 @@ if sudo docker ps -a --format '{{.Names}}' | grep -q "app"; then
 fi
 
 # Run a new container with the name "app"
-docker login -u adnaansidd -p 26122001As@
-sudo docker pull adnaansidd/prod:latest
 sudo docker run -d -p 80:80 --name app adnaansidd/prod:latest
