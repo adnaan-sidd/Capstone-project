8e8c10f6ae99263ce470c1abfe3c51a6db8b48fd
adnaan-sidd <136224753+adnaan-sidd@users.noreply.github.com>
2023-11-10 23:55:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Update package manager
-sudo apt update -y
-
-# Install Docker
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+  # Docker is not installed, so update package manager and install Docker
+  echo "Installing Docker..."
+  sudo apt update -y
+  sudo apt install docker.io -y
+  sudo usermod -aG docker ubuntu
+else
+  echo "Docker is already installed."
+fi
 
 # Check if container with name "app" is running
 if sudo docker ps -a --format '{{.Names}}' | grep -q "app"; then
@@ -14,6 +18,5 @@ if sudo docker ps -a --format '{{.Names}}' | grep -q "app"; then
   sudo docker rm app
 fi
 
-# Run new container with name "app"
+# Run a new container with the name "app"
 sudo docker run -d -p 80:80 --name app adnaansidd/prod:latest
-
